05ff9f62ecf11e355c28889de16a6c1c50d73268
Nicolas Martinez <elnico-2001@hotmail.com>
2023-02-08 19:41:07 -0300

    Add filter for task completed

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -4,8 +4,9 @@ import { useState, useEffect } from "react"
 import { NewTask, Tasks } from '../../components/Tasks'
 
 function Profile(){
-    const [user, setUser] = useState({})
-    const [loading, setLoading] = useState(false)
+    const [ user, setUser ] = useState({})
+    const [ loading, setLoading ] = useState(false)
+    const [ category, setCategory ] = useState(false)
     const navigate = useNavigate()
 
     useEffect(()=> {
@@ -20,11 +21,17 @@ function Profile(){
         redirectUser()
     }, [])
 
-    const handleLogout = () => {
+    const handleLogout = e => {
+        e.preventDefault()
         document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
         navigate('/')
     }
 
+    const changeCategory = (e, state) => {
+        e.preventDefault()
+        if (state) setCategory(!category)
+        else return
+    }
     return(
         loading ?
             <div>
@@ -32,12 +39,16 @@ function Profile(){
                     <div className="container-fluid">
                     <span className="fw-semibold text-truncate" style={{maxWidth: 210 + 'px'}}> Bienvenido {user.user} </span>
                         <div className="d-flex">
-                            <button className="btn btn-outline-danger btn-sm" onClick={handleLogout}>Cerrar Sesión</button>
+                            <button className="btn btn-outline-danger btn-sm" onClick={e => handleLogout(e)}>Cerrar Sesión</button>
                         </div>
                     </div>
                 </header>
                 <NewTask id={user.userId} />
-                <Tasks user={user} />
+                <div className='container mt-4'>
+                    <button className={category? "btn btn-outline-primary btn-sm  me-1" : "btn btn-primary btn-sm me-1"} onClick={e => changeCategory(e, category)}>Sin completar</button>
+                    <button className={!category? "btn btn-outline-primary btn-sm" : "btn btn-primary btn-sm"} onClick={e => changeCategory(e, !category)}>Completadas</button>
+                </div>
+                <Tasks user={user} isCompleted={category} />
             </div>
             :
             <div className="container-fluid d-flex justify-content-center align-items-center" style={{height: 100 + 'vh'}}>
