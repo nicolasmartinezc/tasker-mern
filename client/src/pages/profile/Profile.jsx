72480dc13b17165ac988546f4f45ad895dece71f
Nicolas Martinez <elnico-2001@hotmail.com>
2023-02-08 02:52:44 -0300

    Add loading and content

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,21 +1,50 @@
-import { checkToken } from '../../components/Auth'
+import { CheckToken } from '../../components/Auth'
 import { useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { NewTask, Tasks } from '../../components/Tasks'
 
 function Profile(){
     const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
+
     useEffect(()=> {
         const redirectUser = async() => {
-            const token = await checkToken()
+            const token = await CheckToken()
             if (!token) navigate('/')
-            else setUser(token)
+            else {
+                setTimeout(()=> setLoading(true), 1000)
+                setUser(token)
+            }
         }
         redirectUser()
     }, [])
-    console.log(user);
+
+    const handleLogout = () => {
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+        navigate('/')
+    }
+
     return(
-        <h1>{`Hola mundo ${user.user}`}</h1>
+        loading ?
+            <div>
+                <header className="navbar">
+                    <div className="container-fluid">
+                    <span className="fw-semibold text-truncate" style={{maxWidth: 210 + 'px'}}> Bienvenido {user.user} </span>
+                        <div className="d-flex">
+                            <button className="btn btn-outline-danger btn-sm" onClick={handleLogout}>Cerrar Sesión</button>
+                        </div>
+                    </div>
+                </header>
+                <NewTask id={user.userId} />
+                <Tasks user={user} />
+            </div>
+            :
+            <div className="container-fluid d-flex justify-content-center align-items-center" style={{height: 100 + 'vh'}}>
+                <div className="spinner-border m-5" style={{width: 3 + 'rem', height: 3 + 'rem'}} role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
     )
 }
 
